96e1bd379e42fa51acbdc03f78359f56ee6383ec
Andrew Le <andyle@Andys-Air.lan>
2022-02-21 00:19:34 -0600

    added react-responsive

--- src/styles/index.styles.js
+++ src/styles/index.styles.js
@@ -78,9 +78,9 @@ export const MarqueeContainer = styled.div`
   transform: rotate(20deg);
   filter: brightness(0.75);
 
-  @media only screen and (max-width: 768px) {
-    transform: none;
-  }
+  ${({ isMobile }) => `
+    transform: ${!isMobile ? 'rotate(20deg)' : 'none'};
+  };`}
 `
 
 export const DownloadResume = styled.a`
